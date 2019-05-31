5f1579fa6a403d64ebd276359b78b855ee0737da
Bora Kilicoglu <sbkilicoglu@gmail.com>
2019-05-31 22:41:30 +0200

    Hello Trello

--- src/list/list.controller.ts
+++ src/list/list.controller.ts
@@ -1,4 +1,56 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Delete,
+  UseGuards,
+  Param,
+  Logger,
+  Post,
+  Body,
+  UsePipes,
+  Query,
+} from '@nestjs/common';
 
-@Controller('list')
-export class ListController {}
+import { User } from '../user/user.decorator';
+import { AuthGuard } from '../shared/auth.gaurd';
+import { ValidationPipe } from '../shared/validation.pipe';
+import { ListDTO } from './list.dto';
+import { ListService } from './list.service';
+
+@Controller('api/lists')
+export class ListController {
+  logger = new Logger('ListController');
+  constructor(private listService: ListService) { }
+
+  @Get('board/:id')
+  showCommentsByBoard(@Param('id') name: string, @Query('page') page: number) {
+    return this.listService.showByBoard(board, page);
+  }
+
+  @Post('idea/:id')
+  @UseGuards(new AuthGuard())
+  @UsePipes(new ValidationPipe())
+  createComment(
+    @Param('id') idea: string,
+    @User('id') user: string,
+    @Body() data: ListDTO,
+  ) {
+    return this.commentService.create(idea, user, data);
+  }
+
+  @Get('user/:id')
+  showCommentsByUser(@Param('id') user: string, @Query('page') page: number) {
+    return this.commentService.showByUser(user, page);
+  }
+
+  @Get(':id')
+  showComment(@Param('id') id: string) {
+    return this.commentService.show(id);
+  }
+
+  @Delete(':id')
+  @UseGuards(new AuthGuard())
+  destroyComment(@Param('id') id: string, @User('id') user: string) {
+    return this.commentService.destroy(id, user);
+  }
+}
