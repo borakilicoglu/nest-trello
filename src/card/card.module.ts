1eb6a9f2949fa7473b86e20c99c56207b8d758b5
Bora Kilicoglu <sbkilicoglu@gmail.com>
2019-06-04 17:16:32 +0200

    General Update

--- src/card/card.module.ts
+++ src/card/card.module.ts
@@ -6,12 +6,12 @@ import { UserEntity } from '../user/user.entity';
 import { CardController } from './card.controller';
 import { CardService } from './card.service';
 import { CardEntity } from './card.entity';
-// import { CommentResolver } from './comment.resolver';
+import { CardResolver } from './card.resolver';
 
 @Module({
   imports: [TypeOrmModule.forFeature([CardEntity, ListEntity, UserEntity])],
   controllers: [CardController],
   // providers: [ListService, CommentResolver],
-  providers: [CardService],
+  providers: [CardService, CardResolver],
 })
 export class CardModule { }
--- src/list/list.module.ts
+++ src/list/list.module.ts
@@ -3,6 +3,8 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { BoardEntity } from '../board/board.entity';
 import { UserEntity } from '../user/user.entity';
+import { CardService } from '../card/card.service';
+import { CardEntity } from '../card/card.entity';
 import { ListController } from './list.controller';
 import { ListService } from './list.service';
 import { ListEntity } from './list.entity';
@@ -10,9 +12,9 @@ import { ListResolver } from './list.resolver';
 import { AppGateway } from 'app.gateway';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([ListEntity, BoardEntity, UserEntity])],
+  imports: [TypeOrmModule.forFeature([ListEntity, BoardEntity, CardEntity, UserEntity])],
   controllers: [ListController],
   // providers: [ListService, CommentResolver],
-  providers: [ListService, ListResolver, AppGateway],
+  providers: [ListService, ListResolver, CardService, AppGateway],
 })
 export class ListModule { }
