af85829ba0b03115be6b07c2b37ba262c1cc8542
Bora Kilicoglu <sbkilicoglu@gmail.com>
2019-06-12 11:50:51 +0200

    doing it live

--- src/main.ts
+++ src/main.ts
@@ -15,7 +15,7 @@ async function bootstrap() {
       'http://localhost:8081', // react-native
     ],
   });
-  await app.listen(4000);
+  await app.listen(3000);
   Logger.log(`Server running on http://localhost:${port}`, 'Bootstrap');
 }
 bootstrap();
