3cb5a8df756dc029ba0511c2dd9f3e4070ef9faa
Bora Kilicoglu <sbkilicoglu@gmail.com>
2019-06-12 13:08:42 +0200

    doing it live

--- ormconfig.js
+++ ormconfig.js
@@ -23,15 +23,19 @@ const entitiesDir = isProd ? 'dist' : 'src';
 const migrationsDir = isProd ? 'dist/migration/*.js' : 'src/migration/*.ts';
 
 module.exports = {
+  name: 'default',
   type: 'postgres',
-  entities: [`${__dirname}/${entitiesDir}/**/*.entity.${entitiesExtension}`],
-  url: process.env.DATABASE_URL,
-  migrations: [migrationsDir],
   host: process.env.DATABASE_HOST,
   port: process.env.DATABASE_PORT,
   username: process.env.DATABASE_USERNAME,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_NAME,
+  synchronize: true,
+  dropSchema: false,
+  logging: true,
+  entities: [`${__dirname}/${entitiesDir}/**/*.entity.${entitiesExtension}`],
+  url: process.env.DATABASE_URL,
+  migrations: [migrationsDir],
   cli: {
     migrationsDir: 'src/migration',
   },
