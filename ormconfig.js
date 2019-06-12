62698732cec9cde2f81066cfa6caff506bd54183
Bora Kilicoglu <sbkilicoglu@gmail.com>
2019-06-12 12:55:38 +0200

    doing it live

--- ormconfig.js
+++ ormconfig.js
@@ -1,15 +1,33 @@
-require('dotenv').config();
+// require('dotenv').config();
+
+// module.exports = {
+//   name: 'default',
+//   type: 'postgres',
+//   host: process.env.DATABASE_HOST,
+//   port: process.env.DATABASE_PORT,
+//   username: process.env.DATABASE_USERNAME,
+//   password: process.env.DATABASE_PASSWORD,
+//   database: process.env.DATABASE_NAME,
+//   synchronize: true,
+//   dropSchema: false,
+//   logging: true,
+//   entities: ['src/**/*.entity.ts', 'dist/**/*.entity.js'],
+// };
+
+const env = require('dotenv');
+env.config();
+
+const isProd = process.env.NODE_ENV === 'production';
+const entitiesExtension = isProd ? 'js' : 'ts';
+const entitiesDir = isProd ? 'dist' : 'src';
+const migrationsDir = isProd ? 'dist/migration/*.js' : 'src/migration/*.ts';
 
 module.exports = {
-  name: 'default',
   type: 'postgres',
-  host: process.env.DATABASE_HOST,
-  port: process.env.DATABASE_PORT,
-  username: process.env.DATABASE_USERNAME,
-  password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE_NAME,
-  synchronize: true,
-  dropSchema: false,
-  logging: true,
-  entities: ['src/**/*.entity.ts', 'dist/**/*.entity.js'],
+  entities: [`${__dirname}/${entitiesDir}/**/*.entity.${entitiesExtension}`],
+  url: process.env.DATABASE_URL,
+  migrations: [migrationsDir],
+  cli: {
+    migrationsDir: 'src/migration',
+  },
 };
