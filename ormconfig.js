b1bfdaf721e7b6d1cd6e80b498048d5b3201bc39
Bora Kilicoglu <sbkilicoglu@gmail.com>
2019-06-12 13:00:38 +0200

    doing it live

--- ormconfig.js
+++ ormconfig.js
@@ -27,6 +27,11 @@ module.exports = {
   entities: [`${__dirname}/${entitiesDir}/**/*.entity.${entitiesExtension}`],
   url: process.env.DATABASE_URL,
   migrations: [migrationsDir],
+  host: process.env.DATABASE_HOST,
+  port: process.env.DATABASE_PORT,
+  username: process.env.DATABASE_USERNAME,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
   cli: {
     migrationsDir: 'src/migration',
   },
