4c5107b3d85b1b04fe07ff5c28c5692ed51fd378
Bora Kilicoglu <sbkilicoglu@gmail.com>
2019-06-12 20:31:39 +0200

    Generel Update

--- ormconfig.js
+++ ormconfig.js
@@ -1,19 +1,3 @@
-// require('dotenv').config();
-
-// module.exports = {
-//   name: 'default',
-//   type: 'postgres',
-//   host: process.env.DATABASE_HOST,
-//   port: process.env.DATABASE_PORT,
-//   username: process.env.DATABASE_USERNAME,
-//   password: process.env.DATABASE_PASSWORD,
-//   database: process.env.DATABASE_NAME,
-//   synchronize: true,
-//   dropSchema: false,
-//   logging: true,
-//   entities: ['src/**/*.entity.ts', 'dist/**/*.entity.js'],
-// };
-
 const env = require('dotenv');
 env.config();
 
