2fe08a66d3cc0161f3f7205731ad193a4cb9ade9
Bora Kilicoglu <sbkilicoglu@gmail.com>
2019-06-12 12:46:39 +0200

    doing it live

--- ormconfig.js
+++ ormconfig.js
@@ -4,7 +4,7 @@ module.exports = {
   name: 'default',
   type: 'postgres',
   host: process.env.DATABASE_HOST,
-  port: 5432,
+  port: process.env.DATABASE_PORT,
   username: process.env.DATABASE_USERNAME,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_NAME,
