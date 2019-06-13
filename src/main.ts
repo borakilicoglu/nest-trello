30e485ae8f06df7924d2803beade8421728de79f
Bora Kilicoglu <sbkilicoglu@gmail.com>
2019-06-13 17:42:15 +0200

    Generel Update

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: [
-      'https://borakilicoglu.github.io/angular-trello/',
+      'https://borakilicoglu.github.io',
       'http://localhost:4200', // angular
       'http://localhost:3000', // react
       'http://localhost:8081', // react-native
