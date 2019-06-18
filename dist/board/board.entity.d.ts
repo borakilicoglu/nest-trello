461e2315518277ce277845bfb3878433a7767e29
Bora Kilicoglu <sbkilicoglu@gmail.com>
2019-06-18 15:28:21 +0200

    Generel Update

--- dist/board/board.entity.d.ts
+++
@@ -1,9 +0,0 @@
-import { UserEntity } from '../user/user.entity';
-import { ListEntity } from '../list/list.entity';
-export declare class BoardEntity {
-    id: string;
-    created: Date;
-    name: String;
-    author: UserEntity;
-    lists: ListEntity[];
-}
