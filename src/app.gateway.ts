23b7d4e7a3b37c990ded1df369ad39d418b83d68
Bora Kilicoglu <sbkilicoglu@gmail.com>
2019-06-23 09:52:36 +0200

    Update app.gateway.ts

--- src/app.gateway.ts
+++ src/app.gateway.ts
@@ -6,7 +6,7 @@ import {
   OnGatewayDisconnect,
 } from '@nestjs/websockets';
 
-@WebSocketGateway(4001, { transport: ['websocket'] })
+@WebSocketGateway(8080, { transport: ['websocket'] })
 export class AppGateway implements OnGatewayConnection, OnGatewayDisconnect {
   @WebSocketServer()
   wss;
